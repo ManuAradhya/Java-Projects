f9fc7bd5d4a0bce871a6d7380acd9cce73c18a14
LOKESH <LOKESH@WINCTRL-8MMBLH3>
2022-02-23 22:35:26 +0530

    commit again

--- TestOK/src/Feb_17/Threading.java
+++ TestOK/src/Feb_17/Threading.java
@@ -15,23 +15,24 @@ class B extends Thread{
 		System.out.println("current id of B "+ Thread.currentThread().getId());
 	}
 }
-class D implements Runnable{
+class D implements Runnable{     //runnable interface
 	public void run() {
 		System.out.println("current id of D "+ Thread.currentThread().getId());
 	}
 }
 public class Threading{
 public static void main(String[] args) {
-X x=new X();
-x.start();
+X x=new X();  
+x.start();     //8		
 A a=new A();
-a.start();
+a.start();     //9
 B b=new B();
-b.start();
+b.start();     //10
 D d=new D();
+//d.start();      //Exception error(because we have implemented runnable interface)
 Thread th=new Thread(d);
-th.start();
-System.out.println("ID of main method "+Thread.currentThread().getId());
+th.start();    //11
+System.out.println("ID of main method "+Thread.currentThread().getId());//1
 	}
 
 }
